9f0d8ab384d7f33b34cc1973639fa1f9b5c8d670
knorrkator <knorrkator@posteo.de>
2015-11-15 19:28:53 +0100

    Cleaning up older testruns

--- test.sh
+++ test.sh
@@ -2,16 +2,31 @@
 
 set -e
 
+# Kills a Docker instance if it's running and removes its image.
+# Determines the Docker instance by it's name or id
+kill_and_rm() {
+  local name_or_id=$1
+  docker kill ${name_or_id} > /dev/null 2>&1 || /bin/true
+  docker rm ${name_or_id} > /dev/null 2>&1 || /bin/true
+}
+
+echo Prepare Environment
+kill_and_rm mysql55initial
+kill_and_rm mysql55master
+kill_and_rm mysql55slave
+kill_and_rm mysql55.1
+
+
 echo "=> Building mysql 5.5 image"
 docker build -t mysql-5.5 5.5/
 
 echo "=> Testing if mysql is running on 5.5"
-docker run -d -p 13306:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" mysql-5.5; sleep 10
+docker run -d -p 13306:3306 -e MYSQL_USER="user" -e MYSQL_PASS="test" --name mysql55initial mysql-5.5; sleep 10
 mysqladmin -uuser -ptest -h127.0.0.1 -P13306 ping | grep -c "mysqld is alive"
 
 echo "=> Testing replication on mysql 5.5"
 docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_MASTER=true -e REPLICATION_USER=repl -e REPLICATION_PASS=repl -p 13307:3306 --name mysql55master mysql-5.5; sleep 10
-docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_SLAVE=true -p 13308:3306 --link mysql55master:mysql mysql-5.5; sleep 10
+docker run -d -e MYSQL_USER=user -e MYSQL_PASS=test -e REPLICATION_SLAVE=true -p 13308:3306 --link mysql55master:mysql --name mysql55slave mysql-5.5; sleep 10
 docker logs mysql55master | grep "repl:repl"
 mysql -uuser -ptest -h127.0.0.1 -P13307 -e "show master status\G;" | grep "mysql-bin.*"
 mysql -uuser -ptest -h127.0.0.1 -P13308 -e "show slave status\G;" | grep "Slave_IO_Running.*Yes"
